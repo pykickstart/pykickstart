0cde2eb6b759efcdac49cc45ee8f17356983c2b7
Chris Lumens <clumens@redhat.com>
2005-10-07 20:04:55 +0000

    Bump version.  I can already tell I'm going to forget to do this a lot. Time to look for a fix.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup
 
-setup(name='pykickstart', version='0.2',
+setup(name='pykickstart', version='0.3',
       description='Python module for manipulating kickstart files',
       author='Chris Lumens', author_email='clumens@redhat.com',
       packages=['pykickstart'])
