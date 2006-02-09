381e9e6eef65948aaeb23e7c54b0cf561e71868d
Chris Lumens <clumens@redhat.com>
2006-02-09 18:21:19 +0000

    Condense Option subclasses.

--- pykickstart/parser.py
+++ pykickstart/parser.py
@@ -105,65 +105,58 @@ class KSOptionParser(OptionParser):
     def __init__(self, map={}, lineno=None):
         self.map = map
         self.lineno = lineno
-        OptionParser.__init__(self, option_class=KSBooleanOption,
+        OptionParser.__init__(self, option_class=KSOption,
                               add_help_option=False)
 
-# Creates a new Option type that supports a "required" option attribute.  Any
-# option with this attribute must be supplied or an exception is thrown.
-class RequiredOption (Option):
-    ATTRS = Option.ATTRS + ['required']
+# Creates a new Option class that supports two new attributes:
+# - required:  any option with this attribute must be supplied or an exception
+#              is thrown
+# - deprecated:  any option with this attribute will cause a DeprecationWarning
+#                to be thrown if the option is used
+# Also creates a new type:
+# - ksboolean:  support various kinds of boolean values on an option
+# And two new actions:
+# - map :  allows you to define an opt -> val mapping such that dest gets val
+#          when opt is seen
+# - map_extend:  allows you to define an opt -> [val1, ... valn] mapping such
+#                that dest gets a list of vals built up when opt is seen
+class KSOption (Option):
+    ATTRS = Option.ATTRS + ['deprecated', 'required']
+    ACTIONS = Option.ACTIONS + ("map", "map_extend",)
+    STORE_ACTIONS = Option.STORE_ACTIONS + ("map", "map_extend",)
+    
+    TYPES = Option.TYPES + ("ksboolean",)
+    TYPE_CHECKER = copy(Option.TYPE_CHECKER)
 
-    def _check_required (self):
+    def _check_required(self):
         if self.required and not self.takes_value():
             raise OptionError("Required flag set for option that doesn't take a value", self)
 
+    def _check_ksboolean(option, opt, value):
+        if value.lower() in ("on", "yes", "true", "1"):
+            return True
+        elif value.lower() in ("off", "no", "false", "0"):
+            return False
+        else:
+            raise OptionValueError("option %s: invalid boolean "
+                                   "value: %r" % (opt, value))
+
     # Make sure _check_required() is called from the constructor!
     CHECK_METHODS = Option.CHECK_METHODS + [_check_required]
+    TYPE_CHECKER["ksboolean"] = _check_ksboolean
 
     def process (self, opt, value, values, parser):
         Option.process(self, opt, value, values, parser)
         parser.option_seen[self] = 1
 
-# Additional OptionParser actions.  "map" allows you to define a opt -> val
-# mapping such that dest gets val when opt is seen.  "map_extend" allows you
-# to define an opt -> [val1, ... valn] mapping such that dest gets a list of
-# vals build up when opt is seen.
-class MappableOption(RequiredOption):
-    ACTIONS = RequiredOption.ACTIONS + ("map", "map_extend",)
-    STORE_ACTIONS = RequiredOption.STORE_ACTIONS + ("map", "map_extend",)
-
+    # Override default take_action method to handle our custom actions.
     def take_action(self, action, dest, opt, value, values, parser):
         if action == "map":
             values.ensure_value(dest, parser.map[opt.lstrip('-')])
         elif action == "map_extend":
             values.ensure_value(dest, []).extend(parser.map[opt.lstrip('-')])
         else:
-            RequiredOption.take_action(self, action, dest, opt, value, values, parser)
-
-# Creates a new Option type that supports a "deprecated" option attribute.
-# Any option with this attribute will cause a DeprecationWarning to be
-# thrown if the option is used.
-class DeprecatedOption(MappableOption):
-    ATTRS = MappableOption.ATTRS + ['deprecated']
-
-    def process (self, opt, value, values, parser):
-        MappableOption.process(self, opt, value, values, parser)
-        parser.option_seen[self] = 1
-
-# Creates a new Option type that supports various booleans for values
-class KSBooleanOption(DeprecatedOption):
-    def _check_ksboolean(option, opt, value):
-        if value in ("on", "yes", "1"):
-            return True
-        elif value in ("off", "no", "0"):
-            return False
-        else:
-            raise OptionValueError("option %s: invalid boolean "
-                                   "value: %r" % (opt, value))
-    
-    TYPES = Option.TYPES + ("ksboolean",)
-    TYPE_CHECKER = copy(Option.TYPE_CHECKER)
-    TYPE_CHECKER["ksboolean"] = _check_ksboolean
+            Option.take_action(self, action, dest, opt, value, values, parser)
 
 ###
 ### SCRIPT HANDLING
