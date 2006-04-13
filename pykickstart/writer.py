0916d9f246fdf1fe0d6d7beea90977ff920b06f1
Chris Lumens <clumens@redhat.com>
2006-04-13 15:59:26 +0000

    Reorder package section output.  Write package header options.

--- pykickstart/writer.py
+++ pykickstart/writer.py
@@ -550,15 +550,24 @@ class KickstartWriter:
         if self.ksdata.upgrade:
             return
 
-        retval = "\n%packages\n"
+        retval = "\n%packages"
+
+        if self.ksdata.excludeDocs:
+            retval = retval + " --excludedocs"
+        if not self.ksdata.addBase:
+            retval = retval + " --nobase"
+        if self.ksdata.handleMissing == KS_MISSING_IGNORE:
+            retval = retval + " --ignoremissing"
+
+        retval = retval + "\n"
+
+        for grp in self.ksdata.groupList:
+            retval = retval + "@%s\n" % grp
 
         for pkg in self.ksdata.packageList:
             retval = retval + "%s\n" % pkg
 
         for pkg in self.ksdata.excludedList:
             retval = retval + "-%s\n" % pkg
-
-        for grp in self.ksdata.groupList:
-            retval = retval + "@%s\n" % grp
 
         return retval.rstrip()
