0e9c66b92ed5bcbdc723c92402a1cc841d411850
Chris Lumens <clumens@redhat.com>
2006-12-20 17:41:00 +0000

    Add __doc__ strings to all classes and public methods.  Privatize attributes in KickstartParser that don't need to be public.

--- pykickstart/parser.py
+++ pykickstart/parser.py
@@ -39,13 +39,29 @@ STATE_TRACEBACK = 6
 ###
 ### SCRIPT HANDLING
 ###
-
-# You may make a subclass of Script if you need additional script handling
-# besides just a data representation.  For instance, anaconda may subclass
-# this to add a run method.
 class Script:
+    """A class representing a single kickstart script.  If functionality beyond
+       just a data representation is needed (for example, a run method in
+       anaconda), Script may be subclassed.  Although a run method is not
+       provided, most of the attributes of Script have to do with running the
+       script.  Instances of Script are held in a list by the Handler object.
+    """
     def __init__(self, script, interp = "/bin/sh", inChroot = False,
                  logfile = None, errorOnFail = False, type = KS_SCRIPT_PRE):
+        """Create a new Script instance.  Instance attributes:
+
+           script      -- A string containing all the lines of the script.
+           interp      -- The program that should be used to interpret this
+                          script.
+           inChroot    -- Does the script execute in anaconda's chroot
+                          environment or not?
+           logfile     -- Where all messages from the script should be logged.
+           errorOnFail -- If execution of the script fails, should anaconda
+                          stop, display an error, and then reboot without
+                          running any other scripts?
+           type        -- The type of the script, which can be KS_SCRIPT_* from
+                          pykickstart.constants.
+        """
         self.script = string.join(script, "")
         self.interp = interp
         self.inChroot = inChroot
@@ -53,9 +69,8 @@ class Script:
         self.errorOnFail = errorOnFail
         self.type = type
 
-    # Produce a string representation of the script suitable for writing
-    # to a kickstart file.  Add this to the end of the %whatever header.
     def __str__(self):
+        """Return a string formatted for output to a kickstart file."""
         if self.type == KS_SCRIPT_PRE:
             retval = "%%pre"
         elif self.type == KS_SCRIPT_POST:
@@ -79,7 +94,24 @@ class Script:
 ## PACKAGE HANDLING
 ##
 class Packages:
+    """A class representing the %packages section of the kickstart file."""
     def __init__(self):
+        """Create a new Packages instance.  Instance attributes:
+
+           groupList     -- A list of all the groups specified in the %packages
+                            section, without the leading @ symbol.
+           packageList   -- A list of all the packages specified in the
+                            %packages section.
+           excludedList  -- A list of all the packages marked for exclusion in
+                            the %packages section, without the leading minus
+                            symbol.
+           excludeDocs   -- Should documentation in each package be excluded?
+           addBase       -- Should the Base group be installed even if it is
+                            not specified?
+           handleMissing -- If unknown packages are specified in the %packages
+                            section, should it be ignored or not?  Values can
+                            be KS_MISSING_* from pykickstart.constants.
+        """
         self.groupList = []
         self.packageList = []
         self.excludedList = []
@@ -88,6 +120,7 @@ class Packages:
         self.handleMissing = KS_MISSING_PROMPT
 
     def __str__(self):
+        """Return a string formatted for output to a kickstart file."""
         pkgs = ""
 
         for grp in self.groupList:
@@ -114,6 +147,9 @@ class Packages:
         return retval + "\n" + pkgs
 
     def add (self, pkgList):
+        """Given a list of lines from the input file, strip off any leading
+           symbols and add the result to the appropriate list.
+        """
         for pkg in pkgList:
             stripped = pkg.strip()
 
@@ -129,45 +165,77 @@ class Packages:
 ### PARSER
 ###
 
-# The kickstart file parser.  This only transitions between states and calls
-# handlers at certain points.  To create a specialized parser, make a subclass
-# of this and override the methods you care about.  Methods that don't need to
-# do anything may just pass.
-#
 # Passing None for kshandlers is valid just in case you don't care about
 # handling any commands.
 class KickstartParser:
+    """The kickstart file parser class as represented by a basic state
+       machine.  To create a specialized parser, make a subclass and override
+       any of the methods you care about.  Methods that don't need to do
+       anything may just pass.  However, readKickstart should never be
+       overridden.
+    """
     def __init__ (self, kshandlers, followIncludes=True,
                   errorsAreFatal=True, missingIncludeIsFatal=True):
-        self.handler = kshandlers
+        """Create a new KickstartParser instance.  Instance attributes:
+
+           kshandlers            -- An instance of a BaseHandler subclass.  If
+                                    None, the input file will still be parsed
+                                    but no data will be saved and no command
+                                    handlers will be executed.
+           followIncludes        -- If %include is seen, should the included
+                                    file be checked as well or skipped?
+           errorsAreFatal        -- Should errors cause processing to halt, or
+                                    just print a message to the screen?  This
+                                    is most useful for writing syntax checkers
+                                    that may want to continue after an error is
+                                    encountered.
+           missingIncludeIsFatal -- Should missing include files be fatal, even
+                                    if errorsAreFatal is False?
+        """
+        self.kshandlers = kshandlers
         self.followIncludes = followIncludes
         self.missingIncludeIsFatal = missingIncludeIsFatal
-        self.state = STATE_COMMANDS
-        self.script = None
-        self.includeDepth = 0
         self.errorsAreFatal = errorsAreFatal
+        self._state = STATE_COMMANDS
+        self._script = None
+        self._includeDepth = 0
 
-    # Functions to be called when we are at certain points in the
-    # kickstart file parsing.  Override these if you need special
-    # behavior.
     def addScript (self):
-        if string.join(self.script["body"]).strip() == "":
+        """Create a new Script instance and add it to the Handler object.  This
+           is called when the end of a script section is seen and may be
+           overridden in a subclass if necessary.
+        """
+        if string.join(self._script["body"]).strip() == "":
             return
 
-        s = Script (self.script["body"], self.script["interp"],
-                    self.script["chroot"], self.script["log"],
-                    self.script["errorOnFail"], self.script["type"])
+        s = Script (self._script["body"], self._script["interp"],
+                    self._script["chroot"], self._script["log"],
+                    self._script["errorOnFail"], self._script["type"])
 
-        self.handler.scripts.append(s)
+        if self.kshandlers:
+            self.kshandlers.scripts.append(s)
 
     def addPackages (self, line):
-        self.handler.packages.add([line])
+        """Add the single package, exclude, or group into the Handler's
+           Packages instance.  This method may be overridden in a subclass
+           if necessary.
+        """
+        if self.kshandlers:
+            self.kshandlers.packages.add([line])
 
     def handleCommand (self, lineno, args):
-        if self.handler:
-            self.handler.dispatcher(args[0], args[1:], lineno)
+        """Given the list of command and arguments, call the Handler's
+           dispatcher method to handle the command.  This method may be
+           overridden in a subclass if necessary.
+        """
+        if self.kshandlers:
+            self.kshandlers.dispatcher(args[0], args[1:], lineno)
 
     def handlePackageHdr (self, lineno, args):
+        """Process the arguments to the %packages header and set attributes
+           on the Handler's Packages instance appropriate.  This method may be
+           overridden in a subclass if necessary.
+        """
         op = KSOptionParser(lineno=lineno)
         op.add_option("--excludedocs", dest="excludedocs", action="store_true",
                       default=False)
@@ -182,14 +250,18 @@ class KickstartParser:
 
         (opts, extra) = op.parse_args(args=args[1:])
 
-        self.handler.packages.excludeDocs = opts.excludedocs
-        self.handler.packages.addBase = not opts.nobase
+        self.kshandlers.packages.excludeDocs = opts.excludedocs
+        self.kshandlers.packages.addBase = not opts.nobase
         if opts.ignoremissing:
-            self.handler.packages.handleMissing = KS_MISSING_IGNORE
+            self.kshandlers.packages.handleMissing = KS_MISSING_IGNORE
         else:
-            self.handler.packages.handleMissing = KS_MISSING_PROMPT
+            self.kshandlers.packages.handleMissing = KS_MISSING_PROMPT
 
     def handleScriptHdr (self, lineno, args):
+        """Process the arguments to a %pre/%post/%traceback header for later
+           setting on a Script instance once the end of the script is found.
+           This method may be overridden in a subclass if necessary.
+        """
         op = KSOptionParser(lineno=lineno)
         op.add_option("--erroronfail", dest="errorOnFail", action="store_true",
                       default=False)
@@ -197,22 +269,25 @@ class KickstartParser:
         op.add_option("--log", "--logfile", dest="log")
 
         if args[0] == "%pre" or args[0] == "%traceback":
-            self.script["chroot"] = False
+            self._script["chroot"] = False
         elif args[0] == "%post":
-            self.script["chroot"] = True
+            self._script["chroot"] = True
             op.add_option("--nochroot", dest="nochroot", action="store_true",
                           default=False)
 
         (opts, extra) = op.parse_args(args=args[1:])
 
-        self.script["interp"] = opts.interpreter
-        self.script["log"] = opts.log
-        self.script["errorOnFail"] = opts.errorOnFail
+        self._script["interp"] = opts.interpreter
+        self._script["log"] = opts.log
+        self._script["errorOnFail"] = opts.errorOnFail
         if hasattr(opts, "nochroot"):
-            self.script["chroot"] = not opts.nochroot
+            self._script["chroot"] = not opts.nochroot
 
-    # Parser state machine.  Don't override this in a subclass.
     def readKickstart (self, file):
+        """The kickstart file state machine.  file is the filename to work on.
+           This method should not be overridden as doing so will break the
+           basic kickstart file syntax.
+        """
         # For error reporting.
         lineno = 0
 
@@ -226,18 +301,18 @@ class KickstartParser:
                 needLine = False
 
             # At the end of an included file
-            if line == "" and self.includeDepth > 0:
+            if line == "" and self._includeDepth > 0:
                 fh.close()
                 break
 
             # Don't eliminate whitespace or comments from scripts.
             if line.isspace() or (line != "" and line.lstrip()[0] == '#'):
                 # Save the platform for s-c-kickstart, though.
-                if line[:10] == "#platform=" and self.state == STATE_COMMANDS:
-                    self.handler.platform = line[11:]
+                if line[:10] == "#platform=" and self._state == STATE_COMMANDS:
+                    self.kshandlers.platform = line[11:]
 
-                if self.state in [STATE_PRE, STATE_POST, STATE_TRACEBACK]:
-                    self.script["body"].append(line)
+                if self._state in [STATE_PRE, STATE_POST, STATE_TRACEBACK]:
+                    self._script["body"].append(line)
 
                 needLine = True
                 continue
@@ -245,7 +320,7 @@ class KickstartParser:
             # We only want to split the line if we're outside of a script,
             # as inside the script might involve some pretty weird quoting
             # that shlex doesn't understand.
-            if self.state in [STATE_PRE, STATE_POST, STATE_TRACEBACK]:
+            if self._state in [STATE_PRE, STATE_POST, STATE_TRACEBACK]:
                 # Have we found a state transition?  If so, we still want
                 # to split.  Otherwise, args won't be set but we'll fall through
                 # all the way to the last case.
@@ -266,7 +341,7 @@ class KickstartParser:
                 if not args[1]:
                     raise KickstartParseError, formatErrorMsg(lineno)
                 else:
-                    self.includeDepth += 1
+                    self._includeDepth += 1
 
                     try:
                         self.readKickstart (args[1])
@@ -277,17 +352,17 @@ class KickstartParser:
                         if self.missingIncludeIsFatal:
                             raise
 
-                    self.includeDepth -= 1
+                    self._includeDepth -= 1
                     needLine = True
                     continue
 
-            if self.state == STATE_COMMANDS:
-                if not args and self.includeDepth == 0:
-                    self.state = STATE_END
+            if self._state == STATE_COMMANDS:
+                if not args and self._includeDepth == 0:
+                    self._state = STATE_END
                 elif args[0] in ["%pre", "%post", "%traceback"]:
-                    self.state = STATE_SCRIPT_HDR
+                    self._state = STATE_SCRIPT_HDR
                 elif args[0] == "%packages":
-                    self.state = STATE_PACKAGES
+                    self._state = STATE_PACKAGES
                 elif args[0][0] == '%':
                     # This error is too difficult to continue from, without
                     # lots of resync code.  So just print this one and quit.
@@ -303,11 +378,11 @@ class KickstartParser:
                         except Exception, msg:
                             print msg
 
-            elif self.state == STATE_PACKAGES:
-                if not args and self.includeDepth == 0:
-                    self.state = STATE_END
+            elif self._state == STATE_PACKAGES:
+                if not args and self._includeDepth == 0:
+                    self._state = STATE_END
                 elif args[0] in ["%pre", "%post", "%traceback"]:
-                    self.state = STATE_SCRIPT_HDR
+                    self._state = STATE_SCRIPT_HDR
                 elif args[0] == "%packages":
                     needLine = True
 
@@ -326,22 +401,22 @@ class KickstartParser:
                     needLine = True
                     self.addPackages (string.rstrip(line))
 
-            elif self.state == STATE_SCRIPT_HDR:
+            elif self._state == STATE_SCRIPT_HDR:
                 needLine = True
-                self.script = {"body": [], "interp": "/bin/sh", "log": None,
-                               "errorOnFail": False}
+                self._script = {"body": [], "interp": "/bin/sh", "log": None,
+                                "errorOnFail": False}
 
-                if not args and self.includeDepth == 0:
-                    self.state = STATE_END
+                if not args and self._includeDepth == 0:
+                    self._state = STATE_END
                 elif args[0] == "%pre":
-                    self.state = STATE_PRE
-                    self.script["type"] = KS_SCRIPT_PRE
+                    self._state = STATE_PRE
+                    self._script["type"] = KS_SCRIPT_PRE
                 elif args[0] == "%post":
-                    self.state = STATE_POST
-                    self.script["type"] = KS_SCRIPT_POST
+                    self._state = STATE_POST
+                    self._script["type"] = KS_SCRIPT_POST
                 elif args[0] == "%traceback":
-                    self.state = STATE_TRACEBACK
-                    self.script["type"] = KS_SCRIPT_TRACEBACK
+                    self._state = STATE_TRACEBACK
+                    self._script["type"] = KS_SCRIPT_TRACEBACK
                 elif args[0][0] == '%':
                     # This error is too difficult to continue from, without
                     # lots of resync code.  So just print this one and quit.
@@ -355,22 +430,22 @@ class KickstartParser:
                     except Exception, msg:
                         print msg
 
-            elif self.state in [STATE_PRE, STATE_POST, STATE_TRACEBACK]:
-                if line == "" and self.includeDepth == 0:
+            elif self._state in [STATE_PRE, STATE_POST, STATE_TRACEBACK]:
+                if line == "" and self._includeDepth == 0:
                     # If we're at the end of the kickstart file, add the script.
                     self.addScript()
-                    self.state = STATE_END
+                    self._state = STATE_END
                 elif args and args[0] in ["%pre", "%post", "%traceback", "%packages"]:
                     # Otherwise we're now at the start of the next section.
                     # Figure out what kind of a script we just finished
                     # reading, add it to the list, and switch to the initial
                     # state.
                     self.addScript()
-                    self.state = STATE_COMMANDS
+                    self._state = STATE_COMMANDS
                 else:
                     # Otherwise just add to the current script body.
-                    self.script["body"].append(line)
+                    self._script["body"].append(line)
                     needLine = True
 
-            elif self.state == STATE_END:
+            elif self._state == STATE_END:
                 break
