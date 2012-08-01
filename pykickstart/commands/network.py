ee90c105d227dc77c5356940aac34ab11c747560
Radek Vykydal <rvykydal@redhat.com>
2012-08-01 14:05:49 +0200

    vlan support: add network --vlanid option.
    Resolves: rhbz#668065

--- pykickstart/commands/network.py
+++ pykickstart/commands/network.py
@@ -164,6 +164,7 @@ class RHEL6_NetworkData(F8_NetworkData):
         F8_NetworkData.__init__(self, *args, **kwargs)
         self.activate = kwargs.get("activate", False)
         self.nodefroute = kwargs.get("nodefroute", False)
+        self.vlanid = kwargs.get("vlanid", "")
 
     def _getArgsAsStr(self):
         retval = F8_NetworkData._getArgsAsStr(self)
@@ -172,6 +173,8 @@ class RHEL6_NetworkData(F8_NetworkData):
             retval += " --activate"
         if self.nodefroute:
             retval += " --nodefroute"
+        if self.vlanid:
+            retval += " --vlanid %s" % self.vlanid
 
         return retval
 
@@ -321,4 +324,5 @@ class RHEL6_Network(F9_Network):
                       default=False)
         op.add_option("--nodefroute", dest="nodefroute", action="store_true",
                       default=False)
+        op.add_option("--vlanid", dest="vlanid")
         return op
