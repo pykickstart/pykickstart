f7ff917fc61fd7e8b1ae11f1f70aa0c89bb81740
Chris Lumens <clumens@redhat.com>
2013-06-18 13:14:59 -0400

    Add a new test for the group command.

---
+++ tests/commands/group.py
@@ -0,0 +1,43 @@
+#
+# Chris Lumens <clumens@redhat.com>
+#
+# Copyright 2013 Red Hat, Inc.
+#
+# This copyrighted material is made available to anyone wishing to use, modify,
+# copy, or redistribute it subject to the terms and conditions of the GNU
+# General Public License v.2.  This program is distributed in the hope that it
+# will be useful, but WITHOUT ANY WARRANTY expressed or implied, including the
+# implied warranties of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  Any Red Hat
+# trademarks that are incorporated in the source code or documentation are not
+# subject to the GNU General Public License and may only be used or replicated
+# with the express permission of Red Hat, Inc.
+#
+
+import unittest
+from tests.baseclass import *
+
+class F12_TestCase(CommandTest):
+    command = "user"
+
+    def runTest(self):
+        # pass
+        self.assert_parse("group --name=test", "group --name=test\n")
+        self.assert_parse("group --name=test --gid=1000", "group --name=test --gid=1000\n")
+
+        # fail
+        # missing required option --name
+        self.assert_parse_error("group", KickstartValueError)
+        # --name requires an argument
+        self.assert_parse_error("group --name", KickstartParseError)
+        # --gid requires int argument
+        self.assert_parse_error("group --name=test --uid=id", KickstartParseError)
+        # unknown option
+        self.assert_parse_error("group --name=test --unknown=value", KickstartParseError)
+
+if __name__ == "__main__":
+    unittest.main()
