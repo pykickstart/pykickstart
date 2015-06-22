6f946df753f1d75a5f5293573ab61004f5d45f87
Vratislav Podzimek <vpodzime@redhat.com>
2015-06-22 17:04:56 +0200

    Add options for LVM cache specs to the 'logvol' command
    As the first step towards the LVM cache support we need to teach the parser to
    accept the options for specifying cache size, PVs it should used and mode it
    should use.
    
    Related: rhbz#1120421

--- tests/commands/logvol.py
+++ tests/commands/logvol.py
@@ -320,5 +320,18 @@ class RHEL7_TestCase(F21_TestCase):
         self.assert_parse_error("logvol / --size=4096 --name=LVNAME --vgname=VGNAME --mkfsoptions=some,thing --noformat",
                                 KickstartValueError)
 
+        # accept cache specifications
+        self.assert_parse("logvol /home --name=home --vgname=vg --size=500 --cachesize=250 --cachepvs=pv.01,pv.02 --cachemode=writeback")
+        # cache mode is not required
+        self.assert_parse("logvol /home --name=home --vgname=vg --size=500 --cachesize=250 --cachepvs=pv.01,pv.02")
+
+        # both cache size and cache PVs are required
+        self.assert_parse_error("logvol /home --name=home --vgname=vg --size=500 --cachesize=250")
+        self.assert_parse_error("logvol /home --name=home --vgname=vg --size=500 --cachepvs=pv.01,pv.02")
+        self.assert_parse_error("logvol /home --name=home --vgname=vg --size=500 --cachemode=writeback")
+        self.assert_parse_error("logvol /home --name=home --vgname=vg --size=500 --cachesize=250 --cachepvs=pv.01,pv.02 --cachemode=writeback --useexisting")
+        self.assert_parse_error("logvol /home --name=home --vgname=vg --size=500 --cachesize=250 --cachepvs=pv.01,pv.02 --cachemode=writeback --noformat")
+
+
 if __name__ == "__main__":
     unittest.main()
