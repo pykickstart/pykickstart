061abb47082249b899da464ad70f7c74ea14aa87
Chris Lumens <clumens@redhat.com>
2016-04-12 14:26:10 -0400

    Correctly move scripts after they've been installed.
    The earlier approach breaks running "make install" because it nukes all
    the argument processing required.  This approach hooks into distutils to
    do the moving as part of the build process.

--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
 from distutils.core import setup
-import argparse
+from distutils.command.install_scripts import install_scripts as _install_scripts
+from distutils.file_util import move_file
+
 import os
 
-setup(name='pykickstart', version='2.27',
+class install_scripts(_install_scripts):
+    def run(self):
+        _install_scripts.run(self)
+
+        for old in self.get_outputs():
+            new = os.path.splitext(old)[0]
+            # move_file doesn't have a way to tell it to overwrite, so remove
+            # the existing copy to accomplish the same thing.
+            if os.path.isfile(new) and os.path.exists(new):
+                os.unlink(new)
+            move_file(old, new)
+
+setup(cmdclass={"install_scripts": install_scripts},
+      name='pykickstart', version='2.27',
       description='Python module for manipulating kickstart files',
       author='Chris Lumens', author_email='clumens@redhat.com',
       url='http://fedoraproject.org/wiki/pykickstart',
@@ -10,14 +25,3 @@ setup(name='pykickstart', version='2.27',
       packages=['pykickstart', 'pykickstart.commands', 'pykickstart.handlers'],
       data_files=[('share/man/man1', ['docs/ksvalidator.1', 'docs/ksflatten.1', 'docs/ksverdiff.1',
                                       'docs/ksshell.1'])])
-
-# pykickstart tools were renamed to have .py extension because of code coverage reporting
-# -> we need to rename it afterwards so that the tools don't have the extension after installation
-parser = argparse.ArgumentParser()
-parser.add_argument("action")
-parser.add_argument("--root")
-args = parser.parse_args()
-if args.action == "install":
-    for script in ['ksvalidator.py', 'ksflatten.py', 'ksverdiff.py', 'ksshell.py']:
-        script_path = os.path.join(args.root, "usr", "bin", script)
-        os.rename(script_path, script_path.rstrip(".py"))
