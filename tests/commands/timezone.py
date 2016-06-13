f5720549f50c445708bb0b672f26c34fe4ea538e
Martin Kolman <mkolman@redhat.com>
2016-06-13 20:39:22 +0200

    Support timezone command usage without timezone specification
    Make it possible to use the timezone command without a timezone
    specification.
    
    This way the timezone command can be used with the --utc flag
    to set hardware clock time format while still requiring the user
    to select a timezone manually in the Anaconda UI.
    
    Also throw an exception if only "timezone" is used (eq. no options &
    arguments) as that really does not make sense.
    
    Related: rhbz#1312135

--- tests/commands/timezone.py
+++ tests/commands/timezone.py
@@ -78,5 +78,21 @@ class F18_TestCase(FC6_TestCase):
                                 "ntp.cesnet.cz, tik.nic.cz",
                                 KickstartValueError)
 
+class RHEL7_TestCase(F18_TestCase):
+    def runTest(self):
+        # since RHEL7 command version the timezone command can be used
+        # without a timezone specification
+        self.assert_parse("timezone --utc")
+        self.assert_parse("timezone --isUtc")
+        self.assert_parse("timezone --ntpservers=ntp.cesnet.cz")
+        self.assert_parse("timezone --ntpservers=ntp.cesnet.cz,tik.nic.cz")
+        # unknown argument
+        self.assert_parse_error("timezone --blah")
+        # more than two timezone specs
+        self.assert_parse_error("timezone foo bar", exception=KickstartValueError)
+        self.assert_parse_error("timezone --utc foo bar", exception=KickstartValueError)
+        # just "timezone" without any arguments is also wrong as it really dosn't make sense
+        self.assert_parse_error("timezone")
+
 if __name__ == "__main__":
     unittest.main()
