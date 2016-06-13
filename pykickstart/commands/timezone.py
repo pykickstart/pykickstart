efc6471b121eb3b6ad22fdc726e0c9111cba2e93
Martin Kolman <mkolman@redhat.com>
2016-06-13 20:39:22 +0200

    Support timezone command usage without timezone specification
    Make it possible to use the timezone command without a timezone
    specification.
    
    This way the timezone command can be used with the --utc flag
    to set hardware clock time format while still requiring the user
    to select a timezone manually in the Anaconda UI.
    
    Also throw an exception if only "timezone" is used (eq. no options &
    arguments) as that really does not make sense.
    
    Related: rhbz#1312135

--- pykickstart/commands/timezone.py
+++ pykickstart/commands/timezone.py
@@ -52,6 +52,8 @@ class FC3_Timezone(KickstartCommand):
         op.add_option("--utc", dest="isUtc", action="store_true", default=False)
         return op
 
+    # Caution: This method is overridden in the RHEL7_Timezone & F25_Timezone classes
+    # by a new implementation not calling this method.
     def parse(self, args):
         (opts, extra) = self.op.parse_args(args=args, lineno=self.lineno)
         self.set_to_self(self.op, opts)
@@ -128,6 +130,8 @@ class F18_Timezone(FC6_Timezone):
 
         return op
 
+    # Caution: The parse() method is overridden in the RHEL7_Timezone & F25_Timezone classes
+    # by a new implementation not calling this method.
     def parse(self, args):
         FC6_Timezone.parse(self, args)
 
@@ -142,6 +146,8 @@ class F23_Timezone(F18_Timezone):
         F18_Timezone.__init__(self, *args, **kwargs)
         self.ntpservers = kwargs.get("ntpservers", list())
 
+    # Caution: The parse() method is overridden in the RHEL7_Timezone & F25_Timezone classes
+    # by a new implementation not calling this method.
     def _getParser(self):
         def servers_cb(option, opt_str, value, parser):
             for server in value.split(","):
@@ -154,3 +160,92 @@ class F23_Timezone(F18_Timezone):
                       callback=servers_cb, nargs=1, type="string")
 
         return op
+
+# About the RHEL7_Timezone & F25_Timezone command classes
+# =======================================================
+#
+# The F18_Timezone command class used in RHEL <=7.2 and Fedora <=24 the
+# timezone command always required a ti,ezone specification to be provided.
+#
+# On the other hand the timezone command has also some commands that don't
+# really need a timezone to be specicifed to work, like "--utc/--isutc".
+#
+# So if you for example wanted to set the hwclock to the UTC mode but still
+# wanted the user to set a timezone manually in the UI (by not providing a
+# timezone specification), you were out of luck - a timezone parsing error
+# would be raised.
+#
+# To fix this we need to remove the requirement on always providing one
+# argument to the timezone command. As the requirement is present
+# quite deep in the class hierarchy (in the original FC3_Timezone class)
+# we reimplement the parse() method in the RHEL7_Timezone and F25_Timezone
+# classes and avoid calling the ancestors parse(), as it otherwise done in all
+# other command child classes.
+
+class RHEL7_Timezone(F18_Timezone):
+    def __init__(self, writePriority=0, *args, **kwargs):
+        F18_Timezone.__init__(self, writePriority, *args, **kwargs)
+
+    def parse(self, args):
+        (opts, extra) = self.op.parse_args(args=args, lineno=self.lineno)
+        self._setToSelf(self.op, opts)
+
+        # just "timezone" without any arguments and timezone specification doesn't really make sense,
+        # so throw an error when we see it (it might even be an indication of an incorrect machine generated kickstart)
+        if not args:
+            error_message = _("At least one option and/or an argument are expected for the  %s command") % "timezone"
+            raise KickstartParseError(formatErrorMsg(self.lineno, msg=error_message))
+
+        # To be able to support the timezone command being used without
+        # a timezone specification:
+        # - we don't call the parse() method of the ancestors
+        # -> due to the FC3 parse() method that would be eventually called,
+        #    which throws an exception if no timezone specification is provided
+        # - we implement the relevant functionality of the ancestor methods here
+
+        if len(extra) > 1:
+            error_message = _("One or zero arguments are expected for the %s command") % "timezone"
+            raise KickstartParseError(formatErrorMsg(self.lineno, msg=error_message))
+
+        if len(extra) > 0:
+            self.timezone = extra[0]
+
+        if self.ntpservers and self.nontp:
+            msg = formatErrorMsg(self.lineno, msg=_("Options --nontp and --ntpservers are mutually exclusive"))
+            raise KickstartParseError(msg)
+
+        return self
+
+class F25_Timezone(F18_Timezone):
+    def __init__(self, writePriority=0, *args, **kwargs):
+        F18_Timezone.__init__(self, writePriority, *args, **kwargs)
+
+    def parse(self, args):
+        (opts, extra) = self.op.parse_args(args=args, lineno=self.lineno)
+        self._setToSelf(self.op, opts)
+
+        # just "timezone" without any arguments and timezone specification doesn't really make sense,
+        # so throw an error when we see it (it might even be an indication of an incorrect machine generated kickstart)
+        if not args:
+            error_message = _("At least one option and/or an argument are expected for the  %s command") % "timezone"
+            raise KickstartParseError(formatErrorMsg(self.lineno, msg=error_message))
+
+        # To be able to support the timezone command being used without
+        # a timezone specification:
+        # - we don't call the parse() method of the ancestors
+        # -> due to the FC3 parse() method that would be eventually called,
+        #    which throws an exception if no timezone specification is provided
+        # - we implement the relevant functionality of the ancestor methods here
+
+        if len(extra) > 1:
+            error_message = _("One or zero arguments are expected for the %s command") % "timezone"
+            raise KickstartParseError(formatErrorMsg(self.lineno, msg=error_message))
+
+        if len(extra) > 0:
+            self.timezone = extra[0]
+
+        if self.ntpservers and self.nontp:
+            msg = formatErrorMsg(self.lineno, msg=_("Options --nontp and --ntpservers are mutually exclusive"))
+            raise KickstartParseError(msg)
+
+        return self
