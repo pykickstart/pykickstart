4c57aaf069363b1ae850a5dd1345c34fda3b9118
Alexander Todorov <atodorov@redhat.com>
2016-06-16 12:47:12 +0300

    Add documentation for sshkey

--- pykickstart/commands/sshkey.py
+++ pykickstart/commands/sshkey.py
@@ -17,6 +17,7 @@
 # subject to the GNU General Public License and may only be used or replicated
 # with the express permission of Red Hat, Inc.
 #
+from pykickstart.version import F22
 from pykickstart.base import BaseData, KickstartCommand
 from pykickstart.errors import KickstartParseError, formatErrorMsg
 from pykickstart.options import KSOptionParser
@@ -75,22 +76,34 @@ class F22_SshKey(KickstartCommand):
         return retval
 
     def _getParser(self):
-        op = KSOptionParser()
-        op.add_argument("--username", required=True)
+        op = KSOptionParser(prog="sshkey", description="""
+                            This installs a ssh key to the authorized_keys file
+                            of the specified user on the installed system.""",
+                            epilog="""
+                            Note that the key should be quoted, if it contains
+                            spaces and the user should exist (or be root)
+                            either via creation by a package install or the
+                            kickstart ``user`` command.""", version=F22)
+        op.add_argument("--username", required=True, metavar="<user>",
+                        version=F22, help="""
+                        User for which to install the specified key.""")
+        op.add_argument('sshkey', metavar='"ssh key"', nargs=1,
+                        version=F22, help="""
+                        The content of the ssh key to install.""")
         return op
 
     def parse(self, args):
         ud = self.dataClass()   # pylint: disable=not-callable
         (ns, extra) = self.op.parse_known_args(args=args, lineno=self.lineno)
 
-        if len(extra) != 1:
+        if len(ns.sshkey) != 1:
             raise KickstartParseError(formatErrorMsg(self.lineno, msg=_("A single argument is expected for the %s command") % "sshkey"))
-        elif any(arg for arg in extra if arg.startswith("-")):
+        elif len(extra) > 0:
             mapping = {"command": "sshkey", "options": extra}
             raise KickstartParseError(formatErrorMsg(self.lineno, msg=_("Unexpected arguments to %(command)s command: %(options)s") % mapping))
 
         self.set_to_obj(ns, ud)
-        ud.key = extra[0]
+        ud.key = ns.sshkey[0]
         ud.lineno = self.lineno
 
         if ud in self.dataList():
