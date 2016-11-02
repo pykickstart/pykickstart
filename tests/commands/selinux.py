ed02d66ae7187795918bae8e0bbdf079735715aa
Alexander Todorov <atodorov@redhat.com>
2016-11-02 11:48:53 +0200

    Improve tests for selinux

--- tests/commands/selinux.py
+++ tests/commands/selinux.py
@@ -1,5 +1,6 @@
 import unittest
 from tests.baseclass import CommandTest
+from pykickstart.constants import SELINUX_DISABLED, SELINUX_ENFORCING, SELINUX_PERMISSIVE
 
 class FC3_TestCase(CommandTest):
     command = "selinux"
@@ -17,9 +18,18 @@ class FC3_TestCase(CommandTest):
 
         # extra test coverage
         cmd = self.handler().commands[self.command]
-        cmd.selinux = 999
-        self.assertEqual(cmd.__str__(), "# SELinux configuration\n")
+        for mode in [-1, 99999]:
+            cmd.selinux = mode
+            self.assertEqual(cmd.__str__(), "# SELinux configuration\n")
 
+        cmd.selinux = SELINUX_DISABLED
+        self.assertEqual(cmd.__str__(), "# SELinux configuration\nselinux --disabled\n")
+
+        cmd.selinux = SELINUX_ENFORCING
+        self.assertEqual(cmd.__str__(), "# SELinux configuration\nselinux --enforcing\n")
+
+        cmd.selinux = SELINUX_PERMISSIVE
+        self.assertEqual(cmd.__str__(), "# SELinux configuration\nselinux --permissive\n")
 
 if __name__ == "__main__":
     unittest.main()
