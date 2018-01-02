86d7f78340309ef41b001c0598b1ec1ad75d9945
Chris Lumens <clumens@redhat.com>
2018-01-02 16:16:49 -0500

    Port the F20 firewall test from master.

--- tests/commands/firewall.py
+++ tests/commands/firewall.py
@@ -45,20 +45,6 @@ class FC3_TestCase(CommandTest):
             self.assert_parse("firewall --enable --trust=eth0,eth1 --ssh --http --smtp --ftp --port=1234:udp"
                               "firewall --enabled --port=1234:udp --trust=eth0,eth1 --service=ssh,http,smtp,ftp\n")
 
-        # remove service
-        if "--remove-service" in self.optionList:
-            self.assert_parse("firewall --remove-service=mdns",
-                              "firewall --remove-service=mdns\n")  # remove only
-            # service & remove service at once
-            self.assert_parse("firewall --service=ssh --remove-service=mdns",
-                              "firewall --service=ssh --remove-service=mdns\n")
-            # with alternative service notation
-            self.assert_parse("firewall --ssh --smtp --ftp --remove-service=mdns",
-                              "firewall --ssh --smtp --ftp --remove-service=mdns\n")
-            # multiple remove & remove ssh
-            self.assert_parse("firewall --service=mdns --remove-service=dhcpv6-client --remove-service=ssh",
-                              "firewall --service=mdns --remove-service=dhcpv6-client --remove-service=ssh\n")
-
         # disable firewall
         self.assert_parse("firewall --disabled", "firewall --disabled\n")
         self.assert_parse("firewall --disable", "firewall --disabled\n")
@@ -101,6 +87,23 @@ class F14_TestCase(F10_TestCase):
 
         # removed
         self.assert_removed("firewall", "--telnet")
+
+class F20_TestCase(F14_TestCase):
+    def runTest(self):
+        F14_TestCase.runTest(self)
+
+        # remove service
+        self.assert_parse("firewall --remove-service=mdns",
+                          "firewall --enabled --remove-service=mdns\n")  # remove only
+        # service & remove service at once
+        self.assert_parse("firewall --service=ssh --remove-service=mdns",
+                          "firewall --enabled --service=ssh --remove-service=mdns\n")
+        # with alternative service notation
+        self.assert_parse("firewall --ssh --smtp --ftp --remove-service=mdns",
+                          "firewall --enabled --service=ssh,smtp,ftp --remove-service=mdns\n")
+        # multiple remove & remove ssh
+        self.assert_parse("firewall --service=mdns --remove-service=dhcpv6-client --remove-service=ssh",
+                          "firewall --enabled --service=mdns --remove-service=dhcpv6-client,ssh\n")
 
 if __name__ == "__main__":
     unittest.main()
