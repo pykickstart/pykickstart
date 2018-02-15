ee10bb9a088213e27a8c2dfb8113c5108085fde2
Vendula Poncova <vponcova@redhat.com>
2018-02-15 18:28:09 +0100

    Handle error message formatting in KickstartError
    In anaconda, we need to be able to access the original error
    message and the line number where the error occured. It wasn't
    possible when KickstartError received only the formatted message.
    
    Now, KickstartError handles the error message formatting and it
    keeps the original message and the line number. The function
    formatErrorMsg is deprecated.
    
    The recommended way of creating an exception:
    
        KickstartError(message, lineno=lineno)
    
    The deprecated way is still supported:
    
        KickstartError(formatErrorMsg(message, lineno=lineno))

--- pykickstart/errors.py
+++ pykickstart/errors.py
@@ -20,11 +20,7 @@
 """
 Error handling classes and functions.
 
-This module exports a single function:
-
-    formatErrorMsg - Properly formats an error message.
-
-It also exports several exception classes:
+This module exports several exception classes:
 
     KickstartError - A generic exception class.
 
@@ -36,24 +32,58 @@ It also exports several exception classes:
     KickstartVersionError - An exception for errors relating to unsupported
                             syntax versions.
 """
+import warnings
 from pykickstart.i18n import _
 
+
 def formatErrorMsg(lineno, msg=""):
-    """Properly format the error message msg for inclusion in an exception."""
+    """This function is deprecated. KickstartError formats the error message now,
+       so this function returns a tuple that can be formatted by KickstartError.
+
+       You should call:
+       KickstartError(message, lineno=lineno)
+
+       But the deprecated way is still supported:
+       KickstartError(formatErrorMsg(message, lineno=lineno))
+
+    """
+    warnings.warn("Function formatErrorMsg is deprecated. The error messages "
+                  "are formatted by KickstartError now.", DeprecationWarning)
+
+    return lineno, msg
+
+def _format_error_message(lineno, msg=""):
+    """Properly format the error message msg in an exception.
+       This function should be called only in exceptions to format the error messages.
+    """
     if msg:
-        mapping = {"lineno": lineno, "msg": msg}
-        return _("The following problem occurred on line %(lineno)s of the kickstart file:\n\n%(msg)s\n") % mapping
-    else:
-        return _("There was a problem reading from line %s of the kickstart file") % lineno
+        return _("The following problem occurred on line %(lineno)s of the kickstart file:"
+                 "\n\n%(msg)s\n") % {"lineno": lineno, "msg": msg}
+
+    return _("There was a problem reading from line %s of the kickstart file") % lineno
 
 class KickstartError(Exception):
     """A generic exception class for unspecific error conditions."""
-    def __init__(self, val=""):
+
+    def __init__(self, msg="", lineno=None, formatting=True):
         """Create a new KickstartError exception instance with the descriptive
-           message val.  val should be the return value of formatErrorMsg.
+           message msg. The msg will be formatted if formatting is allowed and
+           the line number lineno is set.
         """
         Exception.__init__(self)
-        self.value = val
+        self.message = msg
+        self.lineno = lineno
+
+        # Accept tuples from formatErrorMsg for backwards compatibility.
+        if isinstance(msg, tuple) and len(msg) == 2:
+            self.lineno, self.message = msg
+
+        # Keep the value attribute for backwards compatibility.
+        self.value = self.message
+
+        # Format the error message if it is allowed.
+        if formatting and self.lineno is not None:
+            self.value = _format_error_message(self.lineno, self.message)
 
     def __str__(self):
         return self.value
@@ -62,37 +92,16 @@ class KickstartParseError(KickstartError):
     """An exception class for errors when processing the input file, such as
        unknown options, commands, or sections.
     """
-    def __init__(self, msg, lineno=None):
-        """Create a new KickstartParseError exception instance with the
-           descriptive message msg.  msg should be the return value of
-           formatErrorMsg.
-        """
-        KickstartError.__init__(self, msg)
-        self.lineno = lineno
-
-    def __str__(self):
-        return self.value
+    pass
 
 class KickstartValueError(KickstartError):
     """This exception class is no longer raised by pykickstart but is kept
        for backwards compatibility.
     """
-    def __init__(self, msg):
-        KickstartError.__init__(self, msg)
-
-    def __str__(self):
-        return self.value
+    pass
 
 class KickstartVersionError(KickstartError):
     """An exception class for errors related to using an incorrect version of
        kickstart syntax.
     """
-    def __init__(self, msg):
-        """Create a new KickstartVersionError exception instance with the
-           descriptive message msg.  msg should be the return value of
-           formatErrorMsg.
-        """
-        KickstartError.__init__(self, msg)
-
-    def __str__(self):
-        return self.value
+    pass
--- tests/errors.py
+++ tests/errors.py
@@ -1,21 +1,109 @@
 import unittest
 from tests.baseclass import ParserTest
 
-from pykickstart.errors import formatErrorMsg, KickstartError, KickstartParseError, KickstartVersionError
+from pykickstart.errors import formatErrorMsg, KickstartError, KickstartParseError, \
+    KickstartVersionError, _format_error_message
 
-class NoErrorMessage_TestCase(ParserTest):
+
+class ErrorMessage_TestCase(ParserTest):
     def runTest(self):
         # For now, just verify that calling formatErrorMsg with no message
         # returns something.  Digging in and checking what the message is
         # when we could be running "make check" in another language is hard.
-        self.assertNotEqual(formatErrorMsg(47), "")
 
-class ExceptionStr_TestCase(ParserTest):
+        # NOTE: formatErrorMsg is replaced with _format_error_message.
+        self.assertNotEqual(_format_error_message(47), "")
+
+        # Function formatErrorMsg is deprecated and returns its arguments now.
+        with self.assertWarns(DeprecationWarning):
+            self.assertEqual(formatErrorMsg(47), (47, ""))
+
+        with self.assertWarns(DeprecationWarning):
+            self.assertEqual(formatErrorMsg(47, "OH NO!"), (47, "OH NO!"))
+
+
+class Exception_TestCase(ParserTest):
     def runTest(self):
-        # Yes, I am aware I'm just checking off boxes now.
         self.assertEqual(str(KickstartError("OH NO!")), "OH NO!")
         self.assertEqual(str(KickstartParseError("OH NO!")), "OH NO!")
         self.assertEqual(str(KickstartVersionError("OH NO!")), "OH NO!")
+
+        err = KickstartError()
+        self.assertEqual(err.message, "")
+        self.assertEqual(err.lineno, None)
+        self.assertEqual(err.value, "")
+        self.assertEqual(str(err), "")
+
+        err = KickstartError("OH NO!")
+        self.assertEqual(err.message, "OH NO!")
+        self.assertEqual(err.lineno, None)
+        self.assertEqual(err.value, "OH NO!")
+        self.assertEqual(str(err), "OH NO!")
+
+        err = KickstartError(lineno=0)
+        self.assertEqual(err.message, "")
+        self.assertEqual(err.lineno, 0)
+        self.assertEqual(err.value, _format_error_message(msg="", lineno=0))
+        self.assertEqual(str(err), _format_error_message(msg="", lineno=0))
+
+        err = KickstartError(lineno=1)
+        self.assertEqual(err.message, "")
+        self.assertEqual(err.lineno, 1)
+        self.assertEqual(err.value, _format_error_message(msg="", lineno=1))
+        self.assertEqual(str(err), _format_error_message(msg="", lineno=1))
+
+        err = KickstartError("OH NO!", lineno=0)
+        self.assertEqual(err.message, "OH NO!")
+        self.assertEqual(err.lineno, 0)
+        self.assertEqual(err.value, _format_error_message(msg="OH NO!", lineno=0))
+        self.assertEqual(str(err), _format_error_message(msg="OH NO!", lineno=0))
+
+        err = KickstartError("OH NO!", lineno=1)
+        self.assertEqual(err.message, "OH NO!")
+        self.assertEqual(err.lineno, 1)
+        self.assertEqual(err.value, _format_error_message(msg="OH NO!", lineno=1))
+        self.assertEqual(str(err), _format_error_message(msg="OH NO!", lineno=1))
+
+        err = KickstartError("OH NO!", lineno=1, formatting=True)
+        self.assertEqual(err.message, "OH NO!")
+        self.assertEqual(err.lineno, 1)
+        self.assertEqual(err.value, _format_error_message(msg="OH NO!", lineno=1))
+        self.assertEqual(str(err), _format_error_message(msg="OH NO!", lineno=1))
+
+        err = KickstartError("OH NO!", lineno=1, formatting=False)
+        self.assertEqual(err.message, "OH NO!")
+        self.assertEqual(err.lineno, 1)
+        self.assertEqual(err.value, "OH NO!")
+        self.assertEqual(str(err), "OH NO!")
+
+        with self.assertWarns(DeprecationWarning):
+            err = KickstartError(formatErrorMsg(lineno=0))
+            self.assertEqual(err.message, "")
+            self.assertEqual(err.lineno, 0)
+            self.assertEqual(err.value, _format_error_message(msg="", lineno=0))
+            self.assertEqual(str(err), _format_error_message(msg="", lineno=0))
+
+        with self.assertWarns(DeprecationWarning):
+            err = KickstartError(formatErrorMsg(lineno=1))
+            self.assertEqual(err.message, "")
+            self.assertEqual(err.lineno, 1)
+            self.assertEqual(err.value, _format_error_message(msg="", lineno=1))
+            self.assertEqual(str(err), _format_error_message(msg="", lineno=1))
+
+        with self.assertWarns(DeprecationWarning):
+            err = KickstartError(formatErrorMsg(msg="OH NO!", lineno=0))
+            self.assertEqual(err.message, "OH NO!")
+            self.assertEqual(err.lineno, 0)
+            self.assertEqual(err.value, _format_error_message(msg="OH NO!", lineno=0))
+            self.assertEqual(str(err), _format_error_message(msg="OH NO!", lineno=0))
+
+        with self.assertWarns(DeprecationWarning):
+            err = KickstartError(formatErrorMsg(msg="OH NO!", lineno=1))
+            self.assertEqual(err.message, "OH NO!")
+            self.assertEqual(err.lineno, 1)
+            self.assertEqual(err.value, _format_error_message(msg="OH NO!", lineno=1))
+            self.assertEqual(str(err), _format_error_message(msg="OH NO!", lineno=1))
+
 
 if __name__ == "__main__":
     unittest.main()
