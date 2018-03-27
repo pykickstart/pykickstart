43064fa9b906a0eb4a8e14e8d17e1f221ae69c11
Radek Vykydal <rvykydal@redhat.com>
2018-03-27 11:32:03 +0200

    Add use action to nvdimm command.
    Related: rhbz#1499442
    
    Nvdimm devices are ignored by default. With this command devices in sector
    mode can be allowed for use in storage configuration.
    NOTE: devices created by reconfigure action are allowed automatically.

--- tests/commands/nvdimm.py
+++ tests/commands/nvdimm.py
@@ -32,14 +32,29 @@ class F28_TestCase(CommandTest):
         self.assert_parse("nvdimm reconfigure --namespace=whatever --mode=sector --sectorsize=512",
                           "nvdimm reconfigure --namespace=whatever --mode=sector --sectorsize=512\n")
 
+        self.assert_parse("nvdimm use --namespace=whatever",
+                          "nvdimm use --namespace=whatever\n")
+        self.assert_parse("nvdimm use --blockdevs=pmem0s1",
+                          "nvdimm use --blockdevs=pmem0s1\n")
+        self.assert_parse("nvdimm use --blockdevs=pmem0s1,pmem0s2",
+                          "nvdimm use --blockdevs=pmem0s1,pmem0s2\n")
+
         # fail
         # --sectorsize is required when recofiguring to sector --mode
         self.assert_parse_error("nvdimm reconfigure --namespace=namespace0.0 --mode=sector")
         self.assert_parse_error("nvdimm reconfigure --namespace=namespace0.0 --mode=invalid --sectorsize=512")
+        # --namespace is requried for reconfigure
         self.assert_parse_error("nvdimm reconfigure --mode=sector --sectorsize=512")
         self.assert_parse_error("nvdimm reconfigure extra --namespace=namespace0.0 --mode=sector --sectorsize=512")
         self.assert_parse_error("nvdimm --namespace=namespace0.0 --mode=sector --sectorsize=512")
         self.assert_parse_error("nvdimm invalid_action --namespace=namespace0.0 --mode=sector --sectorsize=512")
+
+        # Only one of --namespace --blockdevs is allowed
+        self.assert_parse_error("nvdimm reconfigure --namespace=namespace0.0 --blockdevs=pmem0s1 --mode=sector")
+        self.assert_parse_error("nvdimm use --namespace=namespace0.0 --blockdevs=pmem0s1 --mode=sector")
+        # Only --namespace is allowed for reconfigure action
+        self.assert_parse_error("nvdimm reconfigure --blockdev=pmem0s1 --mode=sector")
+
 
 if __name__ == "__main__":
     unittest.main()
