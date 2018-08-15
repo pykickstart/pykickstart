5419ce2c812e13b05fe1d090a3c53748257f1e2f
David Cantrell <dcantrell@redhat.com>
2018-08-15 13:16:04 -0400

    Remove the upgrade command from both F28 and RHEL8.
    Not sure how this got back in, but the upgrade command is gone.

--- pykickstart/handlers/rhel8.py
+++ pykickstart/handlers/rhel8.py
@@ -88,7 +88,6 @@ class RHEL8Handler(BaseHandler):
         "text": commands.displaymode.F26_DisplayMode,
         "timezone": commands.timezone.F25_Timezone,
         "updates": commands.updates.F7_Updates,
-        "upgrade": commands.upgrade.F20_Upgrade,
         "url": commands.url.F27_Url,
         "user": commands.user.F24_User,
         "vnc": commands.vnc.F9_Vnc,
