efac7612dad3cc6b798dc2afda7750cf20526d48
Brian C. Lane <bcl@redhat.com>
2019-11-11 15:07:11 -0800

    Update versionremoved sphinx extension
    Sphinx v2.0.0 added a new mapping, optionally import it and set the
    versionremoved value.
    
    This also returns parallel read/write safe set to True.
    
    Closes #286
    
    (cherry picked from commit 506030dd3d157d2938a12e28b1999a2da23daee5)
    
    Related: rhbz#1776378

--- docs/versionremoved.py
+++ docs/versionremoved.py
@@ -5,6 +5,12 @@ try:
 except ImportError:
     from sphinx.locale import versionlabels
 
+# Sphinx 2.0 added a classname mapping
+try:
+    from sphinx.domains.changeset import versionlabel_classes
+except ImportError:
+    versionlabel_classes = None
+
 from sphinx.directives.other import VersionChange
 
 __version__ = '0.1.0'
@@ -12,10 +18,14 @@ __version__ = '0.1.0'
 def setup(app):
     _directive = 'versionremoved'
 
-    if _directive in versionlabels:
-        return {'version': __version__}
+    if _directive not in versionlabels:
+        versionlabels[_directive] = 'Removed in version %s'
+        if versionlabel_classes is not None:
+            versionlabel_classes[_directive] = "removed"
+        app.add_directive(_directive, VersionChange)
 
-    versionlabels[_directive] = 'Removed in version %s'
-    app.add_directive(_directive, VersionChange)
-
-    return {'version': __version__}
+    return {
+        'version': __version__,
+        'parallel_read_safe': True,
+        'parallel_write_safe': True,
+    }
