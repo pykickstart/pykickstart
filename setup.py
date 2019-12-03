6e53b6135240808e0b38a16711608ce59fbb9661
Brian C. Lane <bcl@redhat.com>
2019-12-03 09:02:55 -0800

    New release: 3.16.9

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ class install_scripts(_install_scripts):
             move_file(old, new)
 
 setup(cmdclass={"install_scripts": install_scripts},
-      name='pykickstart', version='3.16.8',
+      name='pykickstart', version='3.16.9',
       description='Python module for manipulating kickstart files',
       author='Chris Lumens', author_email='clumens@redhat.com',
       url='http://fedoraproject.org/wiki/pykickstart',
