2a9c3be192c3c578a807181f1c8fd27cfb34d1e4
Carlos Meza <carlos@digitalr00ts.com>
2020-05-12 23:23:51 -0700

    move dependencies into setup.py and use setuptools
    resolves #268

--- setup.py
+++ setup.py
@@ -1,28 +1,13 @@
-# pylint: disable=import-error,no-name-in-module
-# See https://github.com/PyCQA/pylint/issues/73#issuecomment-163171888
-from distutils.core import setup
-from distutils.command.install_scripts import install_scripts as _install_scripts
-from distutils.file_util import move_file
+from setuptools import setup
 
-import os
-
-class install_scripts(_install_scripts):
-    def run(self):
-        _install_scripts.run(self)
-
-        for old in self.get_outputs():
-            new = os.path.splitext(old)[0]
-            # move_file doesn't have a way to tell it to overwrite, so remove
-            # the existing copy to accomplish the same thing.
-            if os.path.isfile(new) and os.path.exists(new):
-                os.unlink(new)
-            move_file(old, new)
-
-setup(cmdclass={"install_scripts": install_scripts},
-      name='pykickstart', version='3.27',
+setup(name='pykickstart', version='3.27',
       description='Python module for manipulating kickstart files',
       author='Chris Lumens', author_email='clumens@redhat.com',
       url='http://fedoraproject.org/wiki/pykickstart',
+      install_requires=['six', 'requests'],
+      extras_require={
+          "docs": ['Sphinx'],
+          "test": ['coveralls', 'coverage', 'pocketlint', 'pylint']},
       scripts=['tools/ksvalidator.py', 'tools/ksflatten.py', 'tools/ksverdiff.py', 'tools/ksshell.py'],
       packages=['pykickstart', 'pykickstart.commands', 'pykickstart.handlers'],
       data_files=[('share/man/man1', ['docs/ksvalidator.1', 'docs/ksflatten.1', 'docs/ksverdiff.1',
