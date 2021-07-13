bfd836cfdd8439d984595aca015811ed5c6be733
Brian C. Lane <bcl@redhat.com>
2021-07-13 13:57:35 -0700

    New release: 3.34

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ project = u'Pykickstart'
 copyright = u'2016, Red Hat Installer Engineering Team' # pylint: disable=redefined-builtin
 
 # The short X.Y version.
-version = '3.33'
+version = '3.34'
 # The full version, including alpha/beta/rc tags.
 release = version
 
