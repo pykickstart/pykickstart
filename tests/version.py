6e191093678670b738aa678b66bdc517d4ccda0f
Vladimir Slavik <vslavik@redhat.com>
2022-02-14 11:14:17 +0100

    Add Fedora 36 support

--- tests/version.py
+++ tests/version.py
@@ -140,6 +140,9 @@ class StringToVersion_TestCase(CommandTest):
         # pass - F35
         self.assertEqual(stringToVersion("Fedora 35"), F35)
         self.assertEqual(stringToVersion("F35"), F35)
+        # pass - F36
+        self.assertEqual(stringToVersion("Fedora 36"), F36)
+        self.assertEqual(stringToVersion("F36"), F36)
 
         # pass - RHEL3
         self.assertEqual(stringToVersion("Red Hat Enterprise Linux 3"), RHEL3)
@@ -251,7 +254,8 @@ class VersionToString_TestCase(CommandTest):
         self.assertEqual(versionToString(F33, skipDevel=True), "F33")
         self.assertEqual(versionToString(F34, skipDevel=True), "F34")
         self.assertEqual(versionToString(F35, skipDevel=True), "F35")
-        self.assertEqual(versionToString(F35, skipDevel=False), "DEVEL")
+        self.assertEqual(versionToString(F36, skipDevel=True), "F36")
+        self.assertEqual(versionToString(F36, skipDevel=False), "DEVEL")
         # RHEL series
         self.assertEqual(versionToString(RHEL3), "RHEL3")
         self.assertEqual(versionToString(RHEL4), "RHEL4")
