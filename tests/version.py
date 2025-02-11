e7e2554e1b8cec88018469b1b71def1e8ea933d6
Brian C. Lane <bcl@redhat.com>
2025-02-11 10:46:16 -0800

    Add Fedora 43 support
    Fixes #530

--- tests/version.py
+++ tests/version.py
@@ -161,6 +161,9 @@ class StringToVersion_TestCase(CommandTest):
         # pass - F42
         self.assertEqual(stringToVersion("Fedora 42"), F42)
         self.assertEqual(stringToVersion("F42"), F42)
+        # pass - F43
+        self.assertEqual(stringToVersion("Fedora 43"), F43)
+        self.assertEqual(stringToVersion("F43"), F43)
 
         # pass - RHEL3
         self.assertEqual(stringToVersion("Red Hat Enterprise Linux 3"), RHEL3)
@@ -285,7 +288,8 @@ class VersionToString_TestCase(CommandTest):
         self.assertEqual(versionToString(F40, skipDevel=True), "F40")
         self.assertEqual(versionToString(F41, skipDevel=True), "F41")
         self.assertEqual(versionToString(F42, skipDevel=True), "F42")
-        self.assertEqual(versionToString(F42, skipDevel=False), "DEVEL")
+        self.assertEqual(versionToString(F43, skipDevel=True), "F43")
+        self.assertEqual(versionToString(F43, skipDevel=False), "DEVEL")
         # RHEL series
         self.assertEqual(versionToString(RHEL3), "RHEL3")
         self.assertEqual(versionToString(RHEL4), "RHEL4")
