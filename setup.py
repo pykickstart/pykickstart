bc4ee00cfa7022d535873a0b869c9c1e43cc8feb
Brian C. Lane <bcl@redhat.com>
2025-05-27 10:27:53 -0700

    New release: 3.52.9

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ class install_scripts(_install_scripts):
 
 
 setup(cmdclass={"install_scripts": install_scripts},
-      name='pykickstart', version='3.52.8',
+      name='pykickstart', version='3.52.9',
       description='Python module for manipulating kickstart files',
       author='Chris Lumens', author_email='clumens@redhat.com',
       url='https://fedoraproject.org/wiki/pykickstart',
